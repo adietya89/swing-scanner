ae7710bafa5f14bef8d74bbeaf9f9c473875a679
adietya89 <aplikasikemenkes89@gmail.com>
2026-01-07 15:42:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ for _, row in df.iterrows():
     c1, c2, c3, c4, c5, c6, c7, c8, c9, c10= st.columns(
         [1.2, 1, 1, 1, 1, 0.8, 1, 1, 1, 1.5]
     )
-
+try :
       # Step 1: Ambil data sparkline 90 hari
     close = S(row["_df"]["Close"]).tail(90) 
     close_values = close.to_numpy().squeeze()
@@ -387,6 +387,7 @@ else:
 st.caption(
     f"Update otomatis harian • Last update: {datetime.now().strftime('%d %b %Y %H:%M')}"
 )
+
 
 
 
