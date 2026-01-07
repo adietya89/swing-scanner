a8ff5fa592edfcea4275d70f4e142b3e44404f53
adietya89 <aplikasikemenkes89@gmail.com>
2026-01-07 16:15:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ ROW_HEIGHT = 70
     # =====================
     # Kolom 1 - 9
     # =====================
-   for _, row in df.iterrows():
+for _, row in df.iterrows():
     c1, c2, c3, c4, c5, c6, c7, c8, c9, c10= st.columns(
         [1.2, 1, 1, 1, 1, 0.8, 1, 1, 1, 1]
     )
@@ -371,6 +371,7 @@ else:
 st.caption(
     f"Update otomatis harian • Last update: {datetime.now().strftime('%d %b %Y %H:%M')}"
 )
+
 
 
 
