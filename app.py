2bef2f57aa83e1c8af3d39e48fd7eba401f0e549
adietya89 <aplikasikemenkes89@gmail.com>
2026-01-07 15:46:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,8 @@ for _, row in df.iterrows():
     c1, c2, c3, c4, c5, c6, c7, c8, c9, c10= st.columns(
         [1.2, 1, 1, 1, 1, 0.8, 1, 1, 1, 1.5]
     )
-      # Step 1: Ambil data sparkline 90 hari
+try :  
+    # Step 1: Ambil data sparkline 90 hari
     close = S(row["_df"]["Close"]).tail(90) 
     close_values = close.to_numpy().squeeze()
 
@@ -343,7 +344,10 @@ for _, row in df.iterrows():
         # =========================
     with c10:
          st.altair_chart(chart, use_container_width=True)
-
+except Exception as e:
+    # Jika ada error, tampil "-"
+    with c10:
+        st.write("-")
 # =====================
 # CONFIDENCE METER
 # =====================
@@ -382,6 +386,7 @@ else:
 st.caption(
     f"Update otomatis harian • Last update: {datetime.now().strftime('%d %b %Y %H:%M')}"
 )
+
 
 
 
