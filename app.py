d38e1e0b7de5210f395037fc0c9d7d0092858a7e
adietya89 <aplikasikemenkes89@gmail.com>
2026-01-07 15:36:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -342,13 +342,13 @@ for _, row in df.iterrows():
         # =========================
         # Masukkan ke kolom Sparkline
         # =========================
-     with c10:
-          st.altair_chart(chart, use_container_width=True)
+    with c10:
+         st.altair_chart(chart, use_container_width=True)
 
     except Exception as e:
         # Kalau ada error, tampilkan placeholder
-        with c10:
-            st.write("-")
+    with c10:
+         st.write("-")
 # =====================
 # CONFIDENCE METER
 # =====================
@@ -387,6 +387,7 @@ else:
 st.caption(
     f"Update otomatis harian • Last update: {datetime.now().strftime('%d %b %Y %H:%M')}"
 )
+
 
 
 
