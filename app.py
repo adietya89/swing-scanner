30db7cacc72d939362dfe4f367faf09925b7a428
adietya89 <aplikasikemenkes89@gmail.com>
2026-01-08 16:45:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ st.markdown("""
 }
 .header-box {
     background: linear-gradient(135deg, #0e1117, #151b2c);
-    padding: 28px 24px;
+    padding: 20px;
     border-radius: 16px;
-    margin-top: 10px;
-    margin-bottom: 30px;
+    margin-bottom: 25px;
+}
 [data-testid="stMetric"] {
     background-color: #0e1117;
     padding: 12px;
@@ -542,6 +542,7 @@ else:
 st.caption(
     f"Update otomatis harian • Last update: {datetime.now().strftime('%d %b %Y %H:%M')}"
 )
+
 
 
 
